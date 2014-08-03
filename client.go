83c729a586d0722c09c16e843462e49337fd6e51
tiancaiamao <tiancaiamao@gmail.com>
2014-08-03 23:19:15 +0800

    没造轮子了，善良的国际友人做了很多工作，我没必要瞎折腾了...

--- client.go
+++ client.go
@@ -1,142 +1,14 @@
-package n6bagent
+package main
 
 import (
-    "bufio"
-    "bytes"
     "code.google.com/p/go.net/websocket"
-    "encoding/binary"
-    "errors"
     "github.com/hashicorp/yamux"
-    // "fmt"
     "io"
-    "io/ioutil"
     "log"
     "net"
-    "net/http"
-    // "os"
-    "sync"
 )
 
-type Cell struct {
-    w   http.ResponseWriter
-    r   *http.Request
-    c   chan struct{}
-}
-type Result struct {
-    sync.Mutex
-    kv  map[uint32]*Cell
-}
-
-func newResult() *Result {
-    return &Result{
-        kv: make(map[uint32]*Cell),
-    }
-}
-
-func (r *Result) Put(session uint32, w *Cell) {
-    r.Lock()
-    r.kv[session] = w
-    r.Unlock()
-}
-
-func (r *Result) Del(session uint32) (*Cell, error) {
-    r.Lock()
-    ret, ok := r.kv[session]
-    if !ok {
-        r.Unlock()
-        return nil, errors.New("not exist")
-    }
-    r.Unlock()
-    return ret, nil
-}
-
-func sender(ws *websocket.Conn, ch <-chan []byte) {
-    for {
-        buf := <-ch
-
-        wn := 0
-        total := len(buf)
-        for wn != total {
-            n, err := ws.Write(buf[wn:total])
-            if err != nil {
-                log.Println("handleConn Write error: ", err)
-                return
-            }
-            wn += n
-        }
-    }
-}
-
-func readSession(ws *websocket.Conn, buf *bytes.Buffer) (uint32, error) {
-    var session uint32
-    var size uint32
-
-    err := binary.Read(ws, binary.LittleEndian, &session)
-    if err != nil {
-        return 0, err
-    }
-    binary.Read(ws, binary.LittleEndian, &size)
-    if err != nil {
-        return 0, err
-    }
-
-    buf.Reset()
-    log.Printf("session %d size = %d\n", session, size)
-    _, err = io.CopyN(buf, ws, int64(size))
-    if err != nil {
-        return 0, err
-    }
-    return session, nil
-}
-
-func receiver(ws *websocket.Conn, res *Result) {
-    buf := &bytes.Buffer{}
-    for {
-        session, err := readSession(ws, buf)
-        if err != nil {
-            log.Fatal("readSession error: ", err)
-        }
-
-        c, err := res.Del(session)
-        if err != nil {
-            log.Printf("session %d not exist!!!", session)
-            continue
-        }
-
-        bufreader := bufio.NewReader(buf)
-        resp, err := http.ReadResponse(bufreader, c.r)
-        if err != nil {
-            log.Printf("read response error!!!!")
-            c.c <- struct{}{}
-            continue
-        }
-
-        for k, v := range resp.Header {
-            for _, vv := range v {
-                c.w.Header().Add(k, vv)
-            }
-        }
-
-        result, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            log.Println("读Body也可以出错?", err)
-            c.c <- struct{}{}
-            continue
-        }
-
-        c.w.Write(result)
-        if err != nil {
-            log.Println("写回resp错误：", err)
-        }
-        c.c <- struct{}{}
-    }
-}
-
-type Client struct {
-    multiplex *yamux.Session
-}
-
-func NewClient(hostAddr string) (*Client, error) {
+func NewProxy(hostAddr string) (*yamux.Session, error) {
     origin := "http://" + hostAddr + "/"
     url := "ws://" + hostAddr + "/websocket"
 
@@ -145,60 +17,36 @@ func NewClient(hostAddr string) (*Client, error) {
         return nil, err
     }
 
-    ret := new(Client)
-    ret.multiplex, err = yamux.Client(ws, nil)
-    return ret, err
+    return yamux.Client(ws, nil)
 }
 
-func (c *Client) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    conn, err := c.multiplex.Open()
+func Handle(c *yamux.Session, lconn net.Conn) {
+    rconn, err := c.Open()
     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
+        log.Println("multiplex打开连接失败...")
+        lconn.Close()
         return
     }
 
-    log.Printf("SESSION %s BEGIN: %s %s\n", conn.LocalAddr(), r.Method, r.URL.String())
-
-    w.WriteHeader(200)
-
-    if iconn, _, err := w.(http.Hijacker).Hijack(); err == nil {
-        go copyConn(iconn, conn)
-        go copyConn(conn, iconn)
-    } else {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-
-    log.Printf("SESSION %d END\n", conn.LocalAddr())
+    go io.Copy(rconn, lconn)
+    io.Copy(lconn, rconn)
 }
 
-func copyConn(iconn net.Conn, oconn net.Conn) {
-    buffer := [4096]byte{}
-    defer iconn.Close()
-    defer oconn.Close()
+func main() {
+    proxy, err := NewProxy("n6bagent-c9-tiancaiamao.c9.io:80")
+    // proxy, err := NewProxy("localhost:8080")
+    if err != nil {
+        panic(err)
+    }
+
+    listener, err := net.Listen("tcp", ":48101")
+    if err != nil {
+        panic(err)
+    }
     for {
-        if n, err := iconn.Read(buffer[:]); err == nil {
-            oconn.Write(buffer[:n])
-        } else {
-            return
+        if conn, err := listener.Accept(); err == nil {
+            go Handle(proxy, conn)
         }
     }
-}
 
-// func (s *Server) tunnelTraffic(w http.ResponseWriter, r *http.Request) {
-//     w.WriteHeader(200)
-//
-//     if iconn, _, err := w.(http.Hijacker).Hijack(); err == nil {
-//         proxy := s.getProxy()
-//         log.Printf("socks tunnel by %v: %v", proxy.Addr, r.URL.Host)
-//
-//         if oconn, err := proxy.Dial(Timeout, r.URL.Host); err == nil {
-//             go copyConn(iconn, oconn)
-//             go copyConn(oconn, iconn)
-//         } else {
-//             log.Println("dial socks server %v, error: %v", proxy.Addr, err)
-//             iconn.Close()
-//         }
-//     } else {
-//         http.Error(w, err.Error(), http.StatusInternalServerError)
-//     }
-// }
+}
--- server.go
+++ server.go
@@ -1,18 +1,42 @@
-package n6bagent
+package main
 
 import (
-    "bufio"
-    // "bytes"
     "code.google.com/p/go.net/websocket"
-    // "encoding/binary"
+    "github.com/elazarl/goproxy"
     "github.com/hashicorp/yamux"
     "io"
     "log"
-    "net"
     "net/http"
-    "net/url"
-    "runtime/pprof"
+    _ "net/http/pprof"
 )
+
+func indexCallback(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, html)
+}
+
+func websocketCallback(ws *websocket.Conn) {
+    session, err := yamux.Server(ws, nil)
+    if err != nil {
+        log.Println("websocket serve error:", err)
+        ws.Close()
+        return
+    }
+
+    proxy := goproxy.NewProxyHttpServer()
+    // proxy.Verbose = true
+    err = http.Serve(session, proxy)
+    if err != nil {
+        log.Println("Serve error:", err)
+        ws.Close()
+        return
+    }
+}
+
+func main() {
+    http.HandleFunc("/", indexCallback)
+    http.Handle("/websocket", websocket.Handler(websocketCallback))
+    http.ListenAndServe(":8080", nil)
+}
 
 const html = `
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -31,134 +55,3 @@ const html = `
 </body>
 </html>
 `
-
-type Server struct {
-    *http.ServeMux
-}
-
-func NewServer() *Server {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", indexCallback)
-    mux.Handle("/websocket", websocket.Handler(websocketCallback))
-    mux.HandleFunc("/pprof/goroutine", debugCallback("goroutine"))
-    mux.HandleFunc("/pprof/heap", debugCallback("heap"))
-    mux.HandleFunc("/pprof/threadcreate", debugCallback("threadcreate"))
-    mux.HandleFunc("/pprof/block", debugCallback("block"))
-    return &Server{mux}
-}
-
-// hello world, the web server
-func indexCallback(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, html)
-}
-
-func debugCallback(name string) func(http.ResponseWriter, *http.Request) {
-    return func(w http.ResponseWriter, req *http.Request) {
-        g := pprof.Lookup(name)
-        if g != nil {
-            g.WriteTo(w, 2)
-        } else {
-            io.WriteString(w, html)
-        }
-    }
-}
-
-func worker(conn net.Conn) {
-    log.Println("run here...")
-    bufreader := bufio.NewReader(conn)
-    req, err := http.ReadRequest(bufreader)
-    if err != nil {
-        log.Println("read request error:", conn.RemoteAddr(), err)
-        return
-    }
-    req.URL, err = url.Parse("http://" + req.Host + req.URL.String())
-    req.RequestURI = ""
-
-    log.Printf("SESSION %s BEGIN\n", conn.RemoteAddr())
-
-    if oconn, err := net.DialTimeout("tcp", req.Host, 5); err == nil {
-        go copyConn(conn, oconn)
-        go copyConn(oconn, conn)
-    }
-
-    // if req, err := http.NewRequest(ireq.Method, ireq.URL.String(), ireq.Body); err == nil {
-    //      for k, values := range ireq.Header {
-    //          for _, v := range values {
-    //              req.Header.Add(k, v)
-    //          }
-    //      }
-    //      req.ContentLength = ireq.ContentLength
-    //      // do not follow any redirect， browser will do that
-    //      if resp, err := http.DefaultTransport.RoundTrip(req); err == nil {
-    //          for k, values := range resp.Header {
-    //              for _, v := range values {
-    //                  w.Header().Add(k, v)
-    //              }
-    //          }
-    //          defer resp.Body.Close()
-    //          w.WriteHeader(resp.StatusCode)
-    //          io.Copy(conn, resp.Body)
-    //      }
-    //  }
-
-    log.Printf("SESSION %d END\n", conn.RemoteAddr())
-}
-
-func websocketCallback(ws *websocket.Conn) {
-    session, err := yamux.Server(ws, nil)
-    if err != nil {
-        log.Println("websocket serve error:", err)
-        ws.Close()
-    }
-
-    for {
-        conn, err := session.Accept()
-        if err != nil {
-            log.Println("session Accept error:", err)
-            continue
-        }
-
-        go worker(conn)
-    }
-}
-
-func (s *Server) fetchDirectly(w http.ResponseWriter, ireq *http.Request) {
-    if req, err := http.NewRequest(ireq.Method, ireq.URL.String(), ireq.Body); err == nil {
-        for k, values := range ireq.Header {
-            for _, v := range values {
-                req.Header.Add(k, v)
-            }
-        }
-        req.ContentLength = ireq.ContentLength
-        // do not follow any redirect， browser will do that
-        if resp, err := http.DefaultTransport.RoundTrip(req); err == nil {
-            for k, values := range resp.Header {
-                for _, v := range values {
-                    w.Header().Add(k, v)
-                }
-            }
-            defer resp.Body.Close()
-            w.WriteHeader(resp.StatusCode)
-            io.Copy(w, resp.Body)
-        }
-    }
-}
-
-// func (s *Server) tunnelTraffic(w http.ResponseWriter, r *http.Request) {
-//		 w.WriteHeader(200)
-//
-//		 if iconn, _, err := w.(http.Hijacker).Hijack(); err == nil {
-//				 proxy := s.getProxy()
-//				 log.Printf("socks tunnel by %v: %v", proxy.Addr, r.URL.Host)
-//
-//				 if oconn, err := proxy.Dial(Timeout, r.URL.Host); err == nil {
-//						 go copyConn(iconn, oconn)
-//						 go copyConn(oconn, iconn)
-//				 } else {
-//						 log.Println("dial socks server %v, error: %v", proxy.Addr, err)
-//						 iconn.Close()
-//				 }
-//		 } else {
-//				 http.Error(w, err.Error(), http.StatusInternalServerError)
-//		 }
-// }
